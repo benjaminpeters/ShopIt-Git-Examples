0cebb9ec7a87cea3c546dbd46c781932b85b3531
benjaminpeters <benpeters49@gmail.com>
2015-10-29 13:11:34 -0400

    Todo list using Realm database

---
+++ RealmTasks/RealmTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTasks
+//
+//  Created by Ben Peters on 2015-10-29.
+//  Copyright © 2015 Orange Chips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
